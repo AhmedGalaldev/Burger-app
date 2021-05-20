02ee8e6d821f62316f64f36ad571918114ac48cb
Ahmed Galal <ahmedgalaldev@gmail.com>
2021-05-20 16:52:07 +0200

    add componet should update to Modal.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -5,15 +5,20 @@ import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 import { Component } from "react";
 class Layout extends Component {
   state = {
-    showSideDrawer: true,
+    showSideDrawer: false,
   };
   sideDrawerClosedHandler = () => {
     this.setState({ showSideDrawer: false });
   };
+  sideDrawerToggleHandler = () => {
+    this.setState((prevState) => {
+      return { showSideDrawer: !prevState.showSideDrawer };
+    });
+  };
   render() {
     return (
       <Aux>
-        <Toolbar />
+        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
         <SideDrawer
           open={this.state.showSideDrawer}
           closed={this.sideDrawerClosedHandler}
---
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -0,0 +1,10 @@
+import styles from "./DrawerToggle.module.css";
+const drawerToggle = (props) => (
+  <div onClick={props.clicked} className={styles.DrawerToggle}>
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+);
+
+export default drawerToggle;
